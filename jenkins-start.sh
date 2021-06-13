f8d2dba4782e58d077fd142c17749c0bb8af6e84
glh <glhly@live.com>
2021-06-13 22:07:04 +0800

    注释更新

--- jenkins-start.sh
+++ jenkins-start.sh
@@ -12,8 +12,7 @@
 # ├─ project.jar                                - 当前版本的jar包
 # ├─ jenkins-start.sh                           - jenkins启动脚本
 # └─ log                                        - 日志目录
-#    ├─ project.log                             - 当前版本项目的日志
-#    └─ project.log.yyyy-mm-dd-HH-MM.history    - 旧版本项目的日志
+#    └─ project.log                             - 当前版本项目的日志
 
 ########
 # 配置 #
